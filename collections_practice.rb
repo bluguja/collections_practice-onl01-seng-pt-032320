66434d78e16e786895b4b42337ef51090548f6e8
Bala Luguja <bluguja@gmail.com>
2020-03-30 03:02:37 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -21,8 +21,6 @@ def sort_array_char_count(arr)
 end
 
 #question 4
-#sort_array_char_count(["dogs", "cat", "Horses"]) #["cat", "dogs", "Horses"]
-
 def swap_elements(array)
   array[1], array[2] = array[2], array[1]
   array
